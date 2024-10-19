1b93988179ddb12a6ba50bed2a181f6bec73fd58
Attila Szőke <szokeattila9801@gmail.com>
2024-10-19 22:11:25 +0200

    chore: eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,6 @@
+// @ts-check
+import withNuxt from './.nuxt/eslint.config.mjs';
+// @ts-ignore
+import prettierConfig from '@vue/eslint-config-prettier';
+
+export default withNuxt(prettierConfig);
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: "2024-04-03",
-  devtools: { enabled: true },
-  components: {
-    dirs: [],
-  },
-  pinia: {
-    storesDirs: ["./stores/**"],
-  },
-  modules: ["@pinia/nuxt", "@vueuse/nuxt", "@nuxtjs/i18n"],
+	compatibilityDate: '2024-04-03',
+	devServer: {
+		port: 4567
+	},
+	devtools: { enabled: true },
+	components: {
+		dirs: []
+	},
+	modules: ['@pinia/nuxt', '@vueuse/nuxt', '@nuxtjs/i18n', '@nuxt/eslint'],
+	pinia: {
+		storesDirs: ['./stores/**']
+	}
 });
